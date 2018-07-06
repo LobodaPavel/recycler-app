715ba4ed9115a57620bcac78b8b57b9a4b64c3ec
LobodaPavel <loboda.pavel.and@gmaail.com>
2018-07-06 12:09:49 +0300

    Crash bug

---
+++ app/src/main/java/com/example/pavel/recyclerview/RecyclerViewAdapter.java
@@ -0,0 +1,115 @@
+package com.example.pavel.recyclerview;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.squareup.otto.Bus;
+import com.squareup.otto.Subscribe;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+/**
+ * Created by pavel on 05.07.18.
+ */
+
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>{
+    private static final String TAG = "RecyclerViewAdapter";
+
+    private Bus bus;
+
+    private ArrayList<String> itemText = new ArrayList<>();
+    private ArrayList<String> itemImages = new ArrayList<>();
+    private Context mContext;
+
+    public RecyclerViewAdapter(ArrayList<String> itemText, ArrayList<String> itemImages, Context mContext) {
+        this.itemText = itemText;
+        this.itemImages = itemImages;
+        this.mContext = mContext;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_listitem, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
+        Log.d (TAG, "On bind view holder - > called");
+
+        Glide.with(mContext)
+                .asBitmap()
+                .load(itemImages.get(position))
+                .into(holder.itemImage);
+
+        holder.itemText.setText(itemText.get(position));
+
+        holder.parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Clicked!");
+                Toast.makeText(mContext, "Clicked on: "+itemText.get(position), Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Item : clicked");
+
+                bus = new Bus();
+                bus.register(this);
+
+                Log.d(TAG, "Bus: registered");
+            }
+        });
+
+    }
+
+    private void listenChangeActivity(){
+
+
+        bus.post(new MainActivity());
+
+        Log.d(TAG, "Событие зарегистрировано");
+    }
+    @Subscribe
+    public void changeActivity(MainActivity activity){
+        Log.d(TAG, "Событие начало выполняться");
+        Intent changeActivity = new Intent(mContext, ProfileActivity.class);
+        Log.d(TAG, "activity.startActivity(Intent)");
+        activity.startActivity(changeActivity); // Crash
+        Log.d(TAG, "Событие закончило выполняться");
+    }
+
+    @Override
+    public int getItemCount() {
+        return itemImages.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        CircleImageView itemImage;
+        TextView itemText;
+        RelativeLayout parentLayout;
+
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            itemImage = itemView.findViewById(R.id.item_image);
+            itemText = itemView.findViewById(R.id.item_text);
+            parentLayout = itemView.findViewById(R.id.parent_layout);
+        }
+    }
+}
